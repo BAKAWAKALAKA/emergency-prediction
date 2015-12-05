8eaa1ee2c4f24e3e3e78b94f81a3c8097d4ae6d3
Evgeniya Sheremetova <sheremetova.ei@gmail.com>
2015-12-05 14:03:16 +0300

    OPC types are transformed into doubles

--- src/main/java/ru/spbstu/dis/Tag.java
+++ src/main/java/ru/spbstu/dis/Tag.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 
 public enum Tag {
   PRESSURE,
-  LOWER_PRESSURE;
+  LOWER_PRESSURE,
+  TEST;
 
   public static HashMap<Tag, String> TAG_TO_ID_MAPPING = Maps.newHashMap();
   static {
-    TAG_TO_ID_MAPPING.put(Tag.PRESSURE, "maths.sin");
-    TAG_TO_ID_MAPPING.put(Tag.LOWER_PRESSURE, "maths.cos");
+//    TAG_TO_ID_MAPPING.put(Tag.PRESSURE, "maths.sin");
+//    TAG_TO_ID_MAPPING.put(Tag.LOWER_PRESSURE, "maths.cos");
+    TAG_TO_ID_MAPPING.put(Tag.TEST, "ReactorConnection/M/Temp2");
   }
 }
--- src/main/java/ru/spbstu/dis/opc/OPCDataReader.java
+++ src/main/java/ru/spbstu/dis/opc/OPCDataReader.java
@@ -1,28 +1,15 @@
 package ru.spbstu.dis.opc;
 
 import com.google.common.collect.Maps;
-import org.jinterop.dcom.common.JIException;
-import org.jinterop.dcom.common.JISystem;
-import org.openscada.opc.lib.common.AlreadyConnectedException;
-import org.openscada.opc.lib.common.ConnectionInformation;
-import org.openscada.opc.lib.common.NotConnectedException;
-import org.openscada.opc.lib.da.AccessBase;
-import org.openscada.opc.lib.da.AddFailedException;
-import org.openscada.opc.lib.da.DataCallback;
-import org.openscada.opc.lib.da.DuplicateGroupException;
-import org.openscada.opc.lib.da.Item;
-import org.openscada.opc.lib.da.ItemState;
-import org.openscada.opc.lib.da.Server;
-import org.openscada.opc.lib.da.SyncAccess;
+import org.jinterop.dcom.common.*;
+import org.jinterop.dcom.core.JIVariant;
+import org.openscada.opc.lib.common.*;
+import org.openscada.opc.lib.da.*;
 import ru.spbstu.dis.Tag;
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 public class OPCDataReader {
   private final HashMap<Tag, String> tagsToRead;
@@ -65,21 +52,37 @@ public class OPCDataReader {
     ci.setHost("seal-machine1");
     ci.setUser("Administrator");
     ci.setPassword("seal");
-    //    ci.setClsid("6F17505C-4351-46AC-BC1E-CDE34BB53FAA");
-    ci.setClsid("2E565242-B238-11D3-842D-0008C779D775");
+        ci.setClsid("6F17505C-4351-46AC-BC1E-CDE34BB53FAA");
+//    ci.setClsid("2E565242-B238-11D3-842D-0008C779D775");
 
     final Server server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
     server.connect();
     opcDataAccess = new SyncAccess(server, 200);
-
 
     tagsToRead.forEach((tag, opcId) -> {
       try {
         opcDataAccess.addItem(opcId, new DataCallback() {
           public void changed(Item item, ItemState state) {
             try {
-              final double doubleValuue = state.getValue().getObjectAsDouble();
-              actualValues.put(tag, doubleValuue);
+              final int dataType = state.getValue().getType();
+              switch (dataType) {
+                case JIVariant.VT_BOOL:
+                  final boolean value = state.getValue().getObjectAsBoolean();
+                  if(value){
+                    actualValues.put(tag, 1.0);
+                  } else {
+                    actualValues.put(tag, 0.0);
+                  }
+                  break;
+                case JIVariant.VT_R8:
+                  actualValues.put(tag, .0 + state.getValue().getObjectAsDouble());
+                  break;
+                case JIVariant.VT_R4:
+                  actualValues.put(tag, .0 + state.getValue().getObjectAsFloat());
+                  break;
+                default:
+                  break;
+              }
             } catch (JIException e) {
               e.printStackTrace();
             }
