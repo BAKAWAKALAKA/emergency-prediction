316ff3945b526267415ac3eb8e32a62b6a8a31c6
alex <alexff91@gmail.com>
2016-04-07 08:20:59 +0300

    added items to map

--- src/main/java/ru/spbstu/dis/ep/data/opc/OPCDataReader.java
+++ src/main/java/ru/spbstu/dis/ep/data/opc/OPCDataReader.java
@@ -13,7 +13,7 @@ import java.util.concurrent.Executors;
 
 public class OPCDataReader {
   private final HashMap<Tag, String> tagsToRead;
-
+  private final HashMap<Tag, Item> itemsToRead = Maps.newHashMap();
   private final Map<Tag, Double> actualValues = Maps.newHashMap();
 
   private AccessBase opcDataAccess;
@@ -52,40 +52,38 @@ public class OPCDataReader {
     ci.setHost("seal-machine1");
     ci.setUser("Administrator");
     ci.setPassword("seal");
-//        ci.setClsid("6F17505C-4351-46AC-BC1E-CDE34BB53FAA");
-    ci.setClsid("6F17505C-4351-46AC-BC1E-CDE34BB53FAA");
-
+//        ci.setClsid("6F17505C-4351-46AC-BC1E-CDE34BB53FAA"); --- real server
+    ci.setClsid("2E565242-B238-11D3-842D-0008C779D775");
+    //2E565242-B238-11D3-842D-0008C779D775 --- artificial server
     final Server server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
     server.connect();
     opcDataAccess = new SyncAccess(server, 200);
-
     tagsToRead.forEach((tag, opcId) -> {
       try {
-        opcDataAccess.addItem(opcId, new DataCallback() {
-          public void changed(Item item, ItemState state) {
-            try {
-              final int dataType = state.getValue().getType();
-              switch (dataType) {
-                case JIVariant.VT_BOOL:
-                  final boolean value = state.getValue().getObjectAsBoolean();
-                  if(value){
-                    actualValues.put(tag, 1.0);
-                  } else {
-                    actualValues.put(tag, 0.0);
-                  }
-                  break;
-                case JIVariant.VT_R8:
-                  actualValues.put(tag, .0 + state.getValue().getObjectAsDouble());
-                  break;
-                case JIVariant.VT_R4:
-                  actualValues.put(tag, .0 + state.getValue().getObjectAsFloat());
-                  break;
-                default:
-                  break;
-              }
-            } catch (JIException e) {
-              e.printStackTrace();
+        opcDataAccess.addItem(opcId, (item, state) -> {
+          try {
+            itemsToRead.put(tag, item);
+            final int dataType = state.getValue().getType();
+            switch (dataType) {
+              case JIVariant.VT_BOOL:
+                final boolean value = state.getValue().getObjectAsBoolean();
+                if(value){
+                  actualValues.put(tag, 1.0);
+                } else {
+                  actualValues.put(tag, 0.0);
+                }
+                break;
+              case JIVariant.VT_R8:
+                actualValues.put(tag, .0 + state.getValue().getObjectAsDouble());
+                break;
+              case JIVariant.VT_R4:
+                actualValues.put(tag, .0 + state.getValue().getObjectAsFloat());
+                break;
+              default:
+                break;
             }
+          } catch (JIException e) {
+            e.printStackTrace();
           }
         });
       } catch (JIException e) {
@@ -111,5 +109,9 @@ public class OPCDataReader {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public HashMap<Tag, Item> getItemsToRead() {
+    return itemsToRead;
   }
 }
