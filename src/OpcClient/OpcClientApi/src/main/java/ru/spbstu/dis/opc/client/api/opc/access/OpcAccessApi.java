639660a30ff7f42f9a56b12e6c80d4689a896e10
alex <alexff91@gmail.com>
2016-04-16 19:23:06 +0300

    api updated

--- src/OpcClient/OpcClientApi/src/main/java/ru/spbstu/dis/opc/client/api/opc/access/OpcAccessApi.java
+++ src/OpcClient/OpcClientApi/src/main/java/ru/spbstu/dis/opc/client/api/opc/access/OpcAccessApi.java
@@ -2,11 +2,13 @@ package ru.spbstu.dis.opc.client.api.opc.access;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/api/v1/opc")
@@ -17,23 +19,23 @@ public interface OpcAccessApi {
   @GET
   AvailableTags availableTags();
 
-  @Path("/boolean/{tag}/{value}")
+  @Path("/boolean/{value}")
   @POST
   ValueWritten writeValueForTag(
-      @NotNull @Size(min = 1) @PathParam("tag") String tag,
+      @NotNull @Size(min = 1) @FormParam("tag") String tag,
       @NotNull @PathParam("value") Boolean value);
 
-  @Path("/float/{tag}/{value}")
+  @Path("/float/{value}")
   @POST
   ValueWritten writeValueForTag(
-      @NotNull @Size(min = 1) @PathParam("tag") String tag,
+      @NotNull @Size(min = 1) @FormParam("tag") String tag,
       @NotNull @PathParam("value") Float value);
 
-  @Path("/read/boolean/{tag}")
+  @Path("/read/boolean")
   @GET
-  TagValueBoolean readBoolean(@NotNull @Size(min = 1) @PathParam("tag") String tag);
+  TagValueBoolean readBoolean(@NotNull @Size(min = 1) @QueryParam("tag") String tag);
 
-  @Path("/read/float/{tag}")
+  @Path("/read/float")
   @GET
-  TagValueFloat readFloat(@NotNull @Size(min = 1) @PathParam("tag") String tag);
+  TagValueFloat readFloat(@NotNull @Size(min = 1) @QueryParam("tag") String tag);
 }
--- src/OpcClient/OpcClientApi/src/test/java/ru.spbstu.dis.opc.client.api.opc.access/OpcAccessApiExample.java
+++ src/OpcClient/OpcClientApi/src/test/java/ru.spbstu.dis.opc.client.api.opc.access/OpcAccessApiExample.java
@@ -7,11 +7,18 @@ public class OpcAccessApiExample {
   public static void main(String[] args) {
     final OpcAccessApi opcAccessApi = OpcClientApiFactory
         .createOpcAccessApi(HostAndPort.fromParts("127.0.0.1", 7998));
+    checkPump(opcAccessApi);
+    //    final TagValueFloat tag1 = opcAccessApi.readFloat("tag");
+    //    final ValueWritten valueWritten = opcAccessApi.writeValueForTag("omg", new Float(5.5));
+    //    final ValueWritten omg = opcAccessApi.writeValueForTag("omg", new Boolean(true));
+    //    int i = 0;
+  }
+
+  private static void checkPump(final OpcAccessApi opcAccessApi) {
     final AvailableTags availableTags = opcAccessApi.availableTags();
-    final TagValueBoolean tag = opcAccessApi.readBoolean("tag");
-    final TagValueFloat tag1 = opcAccessApi.readFloat("tag");
-    final ValueWritten valueWritten = opcAccessApi.writeValueForTag("omg", new Float(5.5));
-    final ValueWritten omg = opcAccessApi.writeValueForTag("omg", new Boolean(true));
-    int i = 0;
+    final String pumpName = "MixingConnection/M/TP_2M5";
+    final TagValueBoolean pumpStatus = opcAccessApi.readBoolean(pumpName);
+    final ValueWritten tag = opcAccessApi
+        .writeValueForTag("MixingConnection/M/TP_2M5", !pumpStatus.value);
   }
 }
