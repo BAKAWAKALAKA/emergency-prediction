3335497b207b16f32acd0b7a4b8260f0327474fa
Egor Goloshchapov <golosegor@gmail.com>
2016-04-16 14:39:31 +0300

    swagger support added

--- src/OpcClient/OpcClientServer/src/main/java/ru/spbstu/dis/opc/client/api/http/server/guice/OpcWrapperGuiceModule.java
+++ src/OpcClient/OpcClientServer/src/main/java/ru/spbstu/dis/opc/client/api/http/server/guice/OpcWrapperGuiceModule.java
@@ -2,10 +2,12 @@ package ru.spbstu.dis.opc.client.api.http.server.guice;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.typesafe.config.Config;
 
 public class OpcWrapperGuiceModule implements Module {
 
   public void configure(final Binder binder) {
-
+    binder.bind(Config.class).toProvider(ConfigProvider.class).in(Singleton.class);
   }
 }
